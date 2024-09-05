10001b107e3c08f008d8ed3d1f0c3db26808dab3
RobiulArman <robiularman5@gmail.com>
2024-09-05 10:54:55 +0600

    working on objects

---
+++ string.js
@@ -0,0 +1,13 @@
+// const str = "Hello";
+// str[0] = "h";
+// console.log(str);
+
+// const x = ""
+// if(!x.length){
+// console.log("Hey")
+// }else{
+// console.log("Hi")
+// }
+
+let str = "Mississippi";
+console.log(str.indexOf("i", 3));
