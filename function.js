88872892d60cebd98f4c7845d2e43d8574abf379
RobiulArman <robiularman5@gmail.com>
2024-09-06 16:02:03 +0600

    function

---
+++ function.js
@@ -0,0 +1,11 @@
+
+function money()
+{
+    console.log("Rahim er tk lagbe");
+    console.log("Rahim er tk lagbe");
+    console.log("Rahim er tk lagbe");
+    console.log("Rahim er tk lagbe");
+
+}
+
+money();
