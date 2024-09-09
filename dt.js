f35d368d98591003c5e4b18e59cd07f22449998a
RobiulArman <robiularman5@gmail.com>
2024-09-09 22:19:28 +0600

    add some code

---
+++ dt.js
@@ -0,0 +1,24 @@
+// Numbers:
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+console.log(person.firstName);
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+console.log(cars[0]);
+
+// Date object:
+const date = new Date("2022-03-25");
